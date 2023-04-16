dc5a4aaf521e88e4bd6cd7eac93e01c45c9c69ef
trifonov-yuriy <110669140+trifonov-yuriy@users.noreply.github.com>
2023-04-16 20:47:58 +0300

    Add files via upload

--- tests/AllTests.java
+++ tests/AllTests.java
@@ -1,15 +1,18 @@
 package tests;
+import game.GameWinnerConsolePrinter;
+import game.Player;
 import tests.unit.*;
 
 public class AllTests
 {
     public static void main(String[] args)
     {
-        new DiceImplTest().rollTest();
-        GameTest gameTest = new GameTest();
-        gameTest.testGameForDiceNull();
-        gameTest.testGameForWinnerPrinterNull();
-        gameTest.testGameForWinnerConsolePrinterPlayerNull();
+        new DiceImplTest().testRollForRange();
+        GameTest gameTest = new GameTest(new DiceImplTest(), new GameWinnerConsolePrinter());
+        gameTest.testPlayGameForDiceNull();
+        gameTest.testPlayGameForGameWinnerPrinterNull();
+        gameTest.testGameWinnerConsolePrinterForPlayerNull();
+        gameTest.testPlayGameForPlayerResult(new Player("Игорь"), new Player("Вася"));
     }
 
 }
