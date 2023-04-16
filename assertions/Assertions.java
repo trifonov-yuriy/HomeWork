010082e92477f5370bae208942940a4c843d5319
trifonov-yuriy <110669140+trifonov-yuriy@users.noreply.github.com>
2023-04-16 19:45:43 +0300

    Add files via upload

--- assertions/Assertions.java
+++ assertions/Assertions.java
@@ -1,1 +1,36 @@
+package assertions;
 
+public class Assertions
+{
+    public static void assertEquals(int expected, int actual) {
+        if (expected != actual) {
+            throw new AssertionError(String.format("Expected %d = %d", expected, actual));
+        }
+    }
+
+    public static void assertEquals(long expected, long actual) {
+        if (expected != actual) {
+            throw new AssertionError(String.format("Expected %d = %d", expected, actual));
+        }
+    }
+
+    public static void assertEquals(String expected, String actual) {
+        if (!expected.equals(actual)) {
+            throw new AssertionError(String.format("Expected \"%s\" = \"%s\"", expected, actual));
+        }
+    }
+
+    public static void assertRange(int expected, int actualMin, int actualMax)
+    {
+        if(expected < actualMin || expected > actualMax)
+        {
+            throw new AssertionError(String.format("Expected %s is not in the range [%s-%s]", expected, actualMin, actualMax));
+        }
+    }
+
+    public static void assertNotNull(Object obj)
+    {
+
+    }
+
+}
