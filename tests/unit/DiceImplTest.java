6af1ea47632bd3a50d799589a6958a2f68885182
trifonov-yuriy <110669140+trifonov-yuriy@users.noreply.github.com>
2023-05-01 16:43:16 +0300

    Add files via upload

---
+++ tests/unit/DiceImplTest.java
@@ -0,0 +1,37 @@
+package tests.unit;
+
+import assertions.Assertions;
+import game.*;
+
+import java.util.Random;
+
+public class DiceImplTest implements Dice
+{
+
+    @Override
+    public int roll() {
+        return new Random().nextInt();
+    }
+
+    public void testRollForRange()
+    {
+        String scenario = "Тест метода Dice.roll() (Проверка на диапазон [1-6]) ";
+        Dice dice = new DiceImpl();
+        for(int i = 0; i < 30; i++)
+        {
+            int valueTest = dice.roll();
+            try
+            {
+                Assertions.assertRange(valueTest, 1, 6);
+                System.out.println(String.format("%s пройден", scenario));
+            }
+            catch (Error e)
+            {
+                System.err.println(String.format("%s не пройден: %s", scenario, e.getMessage()));
+                return;
+            }
+        }
+    }
+
+
+}
