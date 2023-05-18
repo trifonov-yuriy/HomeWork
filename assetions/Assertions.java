384d7f44c045a8b50e5be8c1b5ff6852ca661414
trifonov-yuriy <110669140+trifonov-yuriy@users.noreply.github.com>
2023-05-18 22:33:04 +0300

    Add files via upload

--- assetions/Assertions.java
+++ assetions/Assertions.java
@@ -1,1 +1,50 @@
+package assertions;
 
+public class Assertions
+{
+    public static void assertEquals(int expected, int actual) {
+        if (expected != actual) {
+            throw new AssertionError(String.format("Expected %d = %d", expected, actual));
+        }
+    }
+
+    public static void assertEquals(long expected, long actual) {
+        if (expected != actual) {
+            throw new AssertionError(String.format("Expected %d = %d", expected, actual));
+        }
+    }
+
+    public static void assertEquals(String expected, String actual) {
+        if (!expected.equals(actual)) {
+            throw new AssertionError(String.format("Expected \"%s\" = \"%s\"", expected, actual));
+        }
+    }
+
+    public static void assertContains(String expected, String actual) {
+        if (!expected.contains(actual)) {
+            throw new AssertionError(String.format("Expected \"%s\" contains \"%s\"", expected, actual));
+        }
+    }
+
+    public static void assertRange(int expected, int actualMin, int actualMax)
+    {
+        if(expected < actualMin || expected > actualMax)
+        {
+            throw new AssertionError(String.format("Expected %s in the range [%s-%s]", expected, actualMin, actualMax));
+        }
+    }
+
+    public static void assertNotEquals(int expected, int actual)
+    {
+        if(expected == actual)
+        {
+            throw new AssertionError(String.format("Expected \"%s\" != \"%s\"", expected, actual));
+        }
+    }
+
+    public static void assertNotNull(Object obj)
+    {
+
+    }
+
+}
